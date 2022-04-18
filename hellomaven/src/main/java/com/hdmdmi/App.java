c71886819355a558ae7546ba43b0a763f29bda1f
Ajay Lee <ajaidonglee@gmail.com>
2022-04-18 19:53:40 +0530

    day 1

---
+++ hellomaven/src/main/java/com/hdmdmi/App.java
@@ -0,0 +1,11 @@
+package com.hdmdmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ twosum/src/main/java/com/hdmdmi/App.java
@@ -0,0 +1,13 @@
+package com.hdmdmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a + b);
+    }
+}
